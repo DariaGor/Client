6133e9fa7377c3918024a20207476a95cc7704b9
DariaGor <nemesis_97@mail.ru>
2017-05-01 22:59:19 +0300

    готовый клиент

---
+++ Cli_WINforms/MyForm.cpp
@@ -0,0 +1,36 @@
+#include "MyForm.h"
+#include "pass.h"
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+
+
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Cli_WINforms::MyForm form;
+	Application::Run(%form);
+}
+
+
+
+
